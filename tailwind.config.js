11997c3f14416436fe85c675468130579d7ce3b9
Juliene Airen <julieneairenbontilao@gmail.com>
2024-05-19 03:35:54 +0800

    updatep2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
       colors: {
         mydefault: '#F5E6CA'
       },
+
       dropShadow: {
         '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
         '4xl': [
             '0 35px 35px rgba(0, 0, 0, 0.55)',
             '0 45px 65px rgba(0, 0, 0, 0.35)'
         ]
+      },
+
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2'},
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2'},
       }
+
     },
   },
   plugins: [],
