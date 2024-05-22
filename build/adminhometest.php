95ac7ad2e8af73d6a275abfb732e5e64c3e8b7f0
krimazo <kristiannaaamazo@gmail.com>
2024-05-22 22:47:20 +0800

    minor changes on login

--- build/adminhometest.php
+++ build/adminhometest.php
@@ -1,7 +1,28 @@
+<?php
+session_start();
+$name="";
+ $uname = $_SESSION["username"];
+
+switch ($uname) {
+    case "graysee":
+        $name = "Gracie";
+        break;
+    case "juliyenyenn":
+        $name = "Juliene";
+        break;
+    case "kriirmazo":
+        $name = "Kristianna";
+        break;
+    default:
+        $name = "Admin";
+}
+?>
+
 <html> 
     <head></head>
     <body>
         <h1>ADMIN HOMEPAGE!</h1>
+        <h1>Welcome, <?php echo $name?>!</h1>
         <a href ="logoutTest.php">Logout</a>
     </body>
 
--- build/adminlogin.php
+++ build/adminlogin.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $host = "localhost";
 $user = "root";
 $pass = "";
